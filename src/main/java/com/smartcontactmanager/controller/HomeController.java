d9f6d0f87586f9d717845716a3d4c1f9065577c4
Harshit Pachori <harshitpachori345@gmail.com>
2023-04-14 21:10:44 +0530

    added bootstrap , css , js

--- src/main/java/com/smartcontactmanager/controller/HomeController.java
+++ src/main/java/com/smartcontactmanager/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.smartcontactmanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smartcontactmanager.dao.UserRepo;
@@ -21,6 +23,18 @@ public class HomeController {
         user.setName("harshit");
         userRepo.save(user);
         return "working....";
+    }
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("title", "Home - Smart Contact Manager");
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "about";
     }
 
 }
